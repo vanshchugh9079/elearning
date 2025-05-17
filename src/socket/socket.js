e6a62560134358fbe820a7d512d84b15c523d2c6
vanshchugh9079 <141422297+vanshchugh9079@users.noreply.github.com>
2025-05-17 11:41:35 +0530

    Update socket.js

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 // https://elearning-ndyp.onrender.com
-export const socket = io("http://192.168.29.73:3000", {
+export const socket = io("https://elearning-ndyp.onrender.com", {
   transports: ["websocket"], // Ensure WebSocket transport is enabled
   withCredentials:true,
   autoConnect:false
